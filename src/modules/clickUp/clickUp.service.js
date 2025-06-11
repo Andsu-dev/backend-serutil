a2a98b85985ed753819e2f3032ff005c43f8b9b7
AndersonVoeja <Andsu-dev>
2025-06-10 21:35:09 -0300

    feat: Implement ClickUpService for task management with CRUD operations

---
+++ src/modules/clickUp/clickUp.service.js
@@ -0,0 +1,102 @@
+const axios = require("axios");
+const { clickUpConfig } = require("./config/config");
+const ClickUpTask = require("./models/clickUpTask.model");
+const ClickUpTaskRepository = require("./repositories/clickUpTask.repository");
+const {
+  NotFoundError,
+  BadRequestError,
+} = require("../../core/errors/repository.error");
+
+class ClickUpService {
+  constructor() {
+    this.client = axios.create({
+      baseURL: clickUpConfig.baseUrl,
+      headers: {
+        Authorization: clickUpConfig.apiKey,
+        "Content-Type": "application/json",
+      },
+    });
+    this.taskRepository = new ClickUpTaskRepository();
+  }
+
+  async findAll() {
+    const { data } = await this.client.get(
+      `/list/${clickUpConfig.listId}/task`
+    );
+
+    if (!data.tasks) {
+      throw new NotFoundError("Not have tasks");
+    }
+
+    const { tasks } = data;
+
+    const savedTasks = await Promise.all(
+      tasks.map(async (task) => {
+        const doc = ClickUpTask.fromClickUpResponse(task);
+        const savedTask = await this.taskRepository.setDoc(
+          doc?.id,
+          doc.toJSON()
+        );
+        return savedTask;
+      })
+    );
+
+    return savedTasks;
+  }
+  async findOne(taskId) {
+    if (!taskId) {
+      throw new BadRequestError("Task id is required");
+    }
+
+    const { data } = await this.client.get(`/task/${taskId}`);
+
+    if (!data) {
+      throw new NotFoundError("Task not found");
+    }
+
+    const formattedTask = ClickUpTask.fromClickUpResponse(data);
+    return formattedTask.toJSON();
+  }
+
+  async create(taskData) {
+    if (!taskData.name) {
+      throw new BadRequestError("Task name is required");
+    }
+
+    const taskPayload = {
+      name: taskData.name,
+      description: taskData.description || "",
+      status: taskData.status || "to do",
+      start_date: taskData.start_date || null,
+      due_date: taskData.due_date || null,
+    };
+
+    const { data } = await this.client.post(
+      `/list/${clickUpConfig.listId}/task`,
+      taskPayload
+    );
+
+    if (!data) {
+      throw new BadRequestError("error creating task");
+    }
+
+    const doc = ClickUpTask.fromClickUpResponse(data);
+    const savedTask = await this.taskRepository.setDoc(doc?.id, doc.toJSON());
+
+    return { id: savedTask.id };
+  }
+
+  async delete(taskId) {
+    if (!taskId) {
+      throw new BadRequestError("Task ID is required");
+    }
+    const { id } = await this.taskRepository.findById(taskId);
+
+    if (!id) throw new NotFoundError("Task not exist");
+
+    await this.taskRepository.delete(taskId);
+    return { message: "Task deleted successfully" };
+  }
+}
+
+module.exports = ClickUpService;
